e792fa0750ca59acf4de272c233fc4837e2cada5
samyakkhatua <samyakkhatua@duck.com>
2023-06-29 11:29:39 +0530

    Change in Output headline

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function predictCurrent() {
   const temperature = document.getElementById("temperature").value;
   const sunlightIntensity = document.getElementById("sunlight_intensity").value;
   const humidity = document.getElementById("humidity").value;
-  const panelOrientation = document.getElementById("panel_orientation").value;
-  const panelPosition = document.getElementById("panel_position").value;
 
   fetch("http://127.0.0.1:8000/predict", {
     method: "POST",
@@ -13,16 +11,16 @@ function predictCurrent() {
     body: JSON.stringify({
       temperature: temperature,
       sunlight_intensity: sunlightIntensity,
-      humidity: humidity,
-      orientation: panelOrientation,
-      position: panelPosition
+      humidity: humidity
     }),
   })
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById('result').innerHTML = 'Predicted current generated: ' + data.toFixed(2) + ' A';
-    //   document.getElementById("result").innerHTML = data;
-      console.log(data);
+      document.getElementById('current').innerHTML = 'Predicted current generated: ' + data[0].Current_Generated+' A';
+      document.getElementById('Orientation').innerHTML = 'Predicted Panel Orientation: ' + data[0].Panel_Orientation;
+      document.getElementById('Position').innerHTML = 'Predicted Panel Position: ' + data[0].Panel_Position;
+    
+      console.log(data[0].Current_Generated);
     })
     .catch((error) => {
       console.error("Error:", error);
